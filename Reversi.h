1ebd8335f8072472399f5f2d6832ae1c6ddd4a08
Chou Cheng-Yao <anti.ning@gmail.com>
2017-09-20 23:22:07 +0800

    Update Reversi.h

--- Reversi.h
+++ Reversi.h
@@ -1,11 +1,20 @@
 #pragma once
+#include <vector>
+
+//
+const int csquareVal = 10, xsquareVal = 15, cornerVal = 30;
+const int corner[4][2] = { { 1,1 },{ 1,8 },{ 8,1 },{ 8,8 } };
+const int xsquare[4][2] = { {2,2},{2,7},{7,2},{7,7} };
+const int csquare[8][2] = { {1,2},{1,7},{2,1},{2,8},{7,1},{7,8},{8,2},{8,7} };
+const int inf = 1000;
+//
 
 class Reversi {
 private:
 	bool bBW; // b 1, w 0
 	char mArry[64];
 public:
-	enum { eEMPTY, eBLACK, eWHITE };
+	enum { eEMPTY, eBLACK, eWHITE, ePLAYABLE};
 	int numWhite, numBlack;
 
 	void init();
@@ -24,3 +33,6 @@ public:
 	Reversi(void);
 	~Reversi(void);
 };
+
+int evalue(Reversi);
+int minmax(Reversi, int);
