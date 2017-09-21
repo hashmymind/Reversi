697c39ff5d2a195094f0699063787135de67a36a
Chou Cheng-Yao <anti.ning@gmail.com>
2017-09-21 22:39:44 +0800

    Update Reversi.h

--- Reversi.h
+++ Reversi.h
@@ -2,17 +2,18 @@
 #include <vector>
 
 //
-const int sideVal = 4 ,csquareVal = 6, xsquareVal = 7, cornerVal = 30;
+const float sideVal = 4 ,csquareVal = 6, xsquareVal = 7, cornerVal = 30;
 const int corner[4][2] = { { 1,1 },{ 1,8 },{ 8,1 },{ 8,8 } };
 const int xsquare[4][2] = { {2,2},{2,7},{7,2},{7,7} };
 const int csquare[8][2] = { {1,2},{1,7},{2,1},{2,8},{7,1},{7,8},{8,2},{8,7} };
-const int inf = 1000;
+const int inf = 1000000;
 //
 
 class Reversi {
 private:
 	bool bBW; // b 1, w 0
 	char mArry[64];
+	int steps;
 public:
 	enum { eEMPTY, eBLACK, eWHITE, ePLAYABLE};
 	int numWhite, numBlack;
@@ -27,12 +28,13 @@ public:
 	bool isBW();
 	void setBW(int x, int y);
 	char getBW(int x, int y);
-
+	int getSteps();
+	int avaMoving();
 	Reversi operator=(Reversi rhs);
 
 	Reversi(void);
 	~Reversi(void);
 };
 
-int evalue(Reversi);
-int minmax(Reversi, int);
+float evalue(Reversi);
+float minmax(Reversi, int, float, float);
